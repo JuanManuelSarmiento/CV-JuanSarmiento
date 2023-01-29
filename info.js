690e0970e7e071a43e8945336a716badb456ead7
JuanManuelSarmiento <116467738+JuanManuelSarmiento@users.noreply.github.com>
2023-01-29 16:50:19 -0300

    Add files via upload

---
+++ info.js
@@ -0,0 +1,8 @@
+document.getElementById('button').onclick =function ()
+{
+    if (window.confirm("Send message?"))
+    {
+        window.confirm("Thanks for contacting!");
+        location. reload();
+    }
+}
